21439e298267fbe7837d93ba239505f2cbe45a80
Mamithi <19lawrence93@gmail.com>
2017-02-19 00:40:07 +0300

    For and if loops

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue to do app',
-    url: "https://www.google.com"
+    url: "https://www.google.com",
+    todos: [
+      {title: 'Todo 1'},
+      {title: 'Todo 2'},
+      {title: 'Todo 3'},
+      {title: 'Todo 4'},
+      {title: 'Todo 5'}
+    ],
+    showTodos: true
   }
 });
